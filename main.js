114cbd506a0db71df5e8591d9e454e9ff02bf7b9
Luso <lusocmoreira@gmail.com>
2021-06-23 22:44:28 -0300

    update em todos os arquivos

--- main.js
+++ main.js
@@ -26,11 +26,9 @@ for(const link of links) {
 }
 
 /*mudar header quando houver scroll */
-
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
-
-window.addEventListener('scroll', function() {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;  
   if(window.scrollY >= navHeight) {
     // scroll maior que a altura do header
     header.classList.add('scroll');
@@ -38,7 +36,7 @@ window.addEventListener('scroll', function() {
     //menor que a altura do header
     header.classList.remove('scroll');
   }
-})
+}
 
 
 /** 
@@ -64,13 +62,35 @@ const scrollReveal = ScrollReveal({
   distance: '30px',
   duration: 700,
   reset: true
-})
+});
 
 scrollReveal.reveal(`
   #home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   
-  `, { interval: 100})
+  `, { interval: 100});
+
+
+/**
+ * 
+ * BOTAO VOLTAR PARA O TOP
+ * 
+ *  */
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top');  
+  if(window.scrollY >= 560) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+}
+window.addEventListener('scroll', function() {
+  changeHeaderWhenScroll();
+  backToTop();
+});
+
