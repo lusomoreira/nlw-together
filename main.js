3607d4239e97046d349ff212a385fc0c7fcc1af7
Luso <lusocmoreira@gmail.com>
2021-06-25 23:52:47 -0300

    upload final

--- main.js
+++ main.js
@@ -45,12 +45,19 @@ function changeHeaderWhenScroll() {
 */
 
 const swiper = new Swiper('.swiper-container', {
-  slidesPreView: 1,
+  slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination'
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true 
+    }
+  }
+  
 });
 
 
@@ -89,8 +96,38 @@ function backToTop() {
     backToTopButton.classList.remove('show');
   }
 }
+
+/*Menu Ativo conforme a section visivel na pagina*/
+const sections = document.querySelectorAll('main section[id');
+function activateMenuAtCurrentSection() {
+  console.log('entrou aqui');
+  
+  const checkpoint = window.pageYOffset + (window.innerHeight /8)*4;
+
+  for(const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active');
+    } else {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.remove('active');
+    }
+  }
+}
+
+/*when scroll*/
 window.addEventListener('scroll', function() {
   changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
 
